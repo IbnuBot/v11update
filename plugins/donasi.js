e2d6a18b9c42de8fb6f1edab8c895e464fd506e4
IbnuBot <101147082+IbnuBot@users.noreply.github.com>
2022-06-23 06:08:57 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0895338690940]
+│ •  [0895338690940]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
